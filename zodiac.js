e22546d13bec92d827a541ced332c9b6496c4b9d
ChristianJ0 <rojaymerlin@gmail.com>
2024-02-15 19:36:31 +0800

    added features

---
+++ zodiac.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#cv').on('click',function(){
+        window.open("https://drive.google.com/file/d/1lNT3UPQXmUi8lJStqQob3EatziFvJ7KJ/view?usp=sharing");
+    })
+})
